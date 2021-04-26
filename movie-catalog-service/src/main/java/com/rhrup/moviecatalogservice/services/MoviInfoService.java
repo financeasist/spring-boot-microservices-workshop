2de68ca6f7fb970eaacfa6e4f5982b26f67f6969
financeasist <financeasist@gmail.com>
2021-04-26 23:27:31 +0300

    added openFeign

--- movie-catalog-service/src/main/java/com/rhrup/moviecatalogservice/services/MoviInfoService.java
+++ movie-catalog-service/src/main/java/com/rhrup/moviecatalogservice/services/MoviInfoService.java
@@ -5,23 +5,35 @@ import com.rhrup.moviecatalogservice.models.CatalogItem;
 import com.rhrup.moviecatalogservice.models.Movie;
 import com.rhrup.moviecatalogservice.models.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MoviInfoService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private MovieInfoFeignClient movieInfoFeignClient;
+
+    public MoviInfoService(MovieInfoFeignClient movieInfoFeignClient) {
+        this.movieInfoFeignClient = movieInfoFeignClient;
+    }
 
     @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
     public CatalogItem getCatalogItem(Rating rating) {
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+        Movie movie = movieInfoFeignClient.getMovieInfo(rating.getMovieId());
         return new CatalogItem(movie.getName(), movie.getDesc(), rating.getRatingValue());
     }
 
     private CatalogItem getFallbackCatalogItem(Rating rating) {
         return new CatalogItem("Movie name not found", "", rating.getRatingValue());
+    }
+
+    @FeignClient(value = "movie-info-service")
+    static interface MovieInfoFeignClient {
+        @GetMapping("/movies/{movieId}")
+        public Movie getMovieInfo(@PathVariable("movieId") String movieId);
     }
 }
